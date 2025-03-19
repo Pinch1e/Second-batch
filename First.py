d508f63d9f06ab755b875bc9d67351ad05bba5c0
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-19 16:51:33 +0100

    Me

---
+++ First.py
@@ -0,0 +1,11 @@
+a = "Hello, World!"
+print(len(a))
+
+#Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+
+#Using If Statement
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
