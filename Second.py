dc9411ed428f5f4fcba7cd731a89035a5498b619
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-20 16:24:23 +0100

    Me

---
+++ Second.py
@@ -0,0 +1,24 @@
+#Upper Case
+a = "Hello, World!"
+print(a.upper())
+
+#Lower Caase
+a = "Hello World"
+print(a.lower())
+
+#Remove Whitespaces
+a = " Hello, World! "
+print(a.strip())
+
+#Replace String
+a = "Hello String "
+print(a.replace("H", "J"))
+
+#Place holders
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+#Escape Character
+txt = "We are the so called \"Vikings\" from the north"
+print(txt)
