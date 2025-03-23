cc20bccc10ebae16942f8289eddbabadc2a4ec14
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-23 17:47:32 +0100

    Me

--- Sixth.py
+++ Sixth.py
@@ -3,3 +3,12 @@ import os
 Sabi = "New_folder"
 os.makedirs("new_folder", exist_ok=True)
 print(f"Folder '{"sab"}' created successfully!")
+
+#List Comprehension
+Panther = ["Batiboy", "Juvenile", "Dada", "Dracula"]
+newlist = []
+for x in Panther:
+    if "e" in x:
+        print(newlist.append(x))
+
+print(newlist)
