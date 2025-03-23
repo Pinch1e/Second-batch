e30047faf029b9794d8a7e384fb7b8ae4be08316
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-23 17:24:10 +0100

    Me

--- Fifth.py
+++ Fifth.py
@@ -1,3 +1,14 @@
 #Create a new file
-f = open("master.txt", "x")
+f = open("master.txt")
 f = "This is a new Text file created here to see how t goes and build my basic python skills"
+
+#AAppending a List
+thislist = ["Cherry", "Winter", "Calm"]
+thislist.append("Jabulani")
+print(thislist)
+
+#Extending a List
+ort = [25, 30, 45]
+prt = [56, 54, 78]
+ort.extend(prt)
+print(ort)
