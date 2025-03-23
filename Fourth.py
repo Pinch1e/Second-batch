f5f8b6e8a4bec2a8eceedd0fc690b19812525cc4
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-23 16:57:32 +0100

    Me

--- Fourth.py
+++ Fourth.py
@@ -5,3 +5,15 @@ print(f.read())
 #Read only parts of the file
 f = open("pinchie.txt")
 print(f.read(4))
+
+#Read Lines
+f = open("pinchie.txt")
+print(f.readline())
+f.close()
+
+#Append
+f = open("pinchie.txt", "a")
+f.write("Now the files has more content")
+f.close()
+
+print(f)
