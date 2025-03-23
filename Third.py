0c3906d83e74c43613f56f9228099c12d25967ea
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-23 15:51:18 +0100

    Me

--- Third.py
+++ Third.py
@@ -3,3 +3,14 @@ price = 59
 tax = 0.39
 txt =  f"The price is {price + (price * tax )} is dollars"
 print(txt)
+
+#F-Strings
+def myconverter(x):
+    return x * 0.3048
+txt = f"The plane is flying at {myconverter(30000)} meter altitude"
+print(txt)
+
+#Using Comma as a thousand Seperator
+price = 59000
+txt = f"The price is {price:,} dollars"
+print(txt)
