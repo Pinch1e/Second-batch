770accf934e54cfc79673eeaca9c91d21ff43733
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-25 16:53:15 +0100

    Me

--- Eight.py
+++ Eight.py
@@ -1,5 +1,19 @@
 #List Comprehension
+from IPython.terminal.interactiveshell import black_reformat_handler
+from pygments.styles.dracula import yellow
 
 fruits = ["apple", "banana", "cherry"]
 newlist = [x for x in fruits]
 print(newlist)
+
+#Tuple
+thisti = ("cherry",)
+print(type(thisti))
+#the comma differentiates a str from a tuple
+
+#Unpacking Tuples
+fruits = ("cherry", "grape", "blackcurrant")
+(green, yellow, black) = fruits
+print(green)
+print(yellow)
+print(black)
