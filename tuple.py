e6bd5075c83cde54c044469538032c4eb4903fdb
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-05 16:43:09 +0200

    Me

--- tuple.py
+++ tuple.py
@@ -14,3 +14,12 @@ machine = ("great", "are", "the", "days")
 jinga = machine * 2
 print(jinga)
 
+mytuple = ('apple', 'banana', 'cherry')
+i = 0
+while i < len(mytuple):
+  print(mytuple[i])
+  i = i + 1
+
+#True and 1 are the same values on a set
+thisset = {"apple", "banana", "cherry", True, 1, 2}
+print(thisset)
