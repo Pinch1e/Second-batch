f67573723fd37756bb764e92992c2bf2ed5cbda8
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-08 18:03:50 +0200

    Me

---
+++ if.py
@@ -0,0 +1,22 @@
+#IF / ELSE STATEMNENT IN PYTHON
+#a == b Equals
+#a != b Not Equals
+
+a = 33
+b = 242
+if b > a :
+    print("b is greater than a")
+
+#Elif Condition
+a = 33
+b = 33
+if b > a :
+    print("b is greater than a")
+elif b == a :
+    print("b and a are equal")
+else :
+    print("a is greater than b")
+
+#One line statement
+if a > b: ("print a is greater than b")
+
