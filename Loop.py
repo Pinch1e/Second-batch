f5554ee7b5c129e3fb7b46d7a63dfd053c938337
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-08 17:58:44 +0200

    Me

--- Loop.py
+++ Loop.py
@@ -3,3 +3,15 @@ x = {'type' : 'fruit', 'name' : 'apple'}
 for y, z in x.items():
     print(y,z)
 
+#Copying
+y = x.copy()
+print(y)
+
+#Nested Dictionaries
+a = {'name' : 'John', 'age' : '20'}
+b = {'name' : 'May', 'age' : '23'}
+customers = {'c1' : a, 'c2' : b}
+print(customers)
+print(customers['c2']['name'])
+
+f = open("if.py", "x")
