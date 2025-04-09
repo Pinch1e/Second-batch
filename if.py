cc232b7da02bfffe4972dd33745480628a7966dd
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-09 12:32:38 +0200

    Me

--- if.py
+++ if.py
@@ -20,3 +20,20 @@ else :
 #One line statement
 if a > b: ("print a is greater than b")
 
+#Match
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
