73ad379cf294798a138d0fab2ac1dca07aebff7b
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-16 14:48:07 +0200

    Me

--- Iterators.py
+++ Iterators.py
@@ -1,5 +1,4 @@
 #The iterators like the name goes through all the numbers in an object
-
 myguy = ("apple", "Banana", "guava")
 myit = iter(myguy)
 
@@ -11,18 +10,32 @@ class JamboNumbers:
     def __iter__(self):
         self.a = 1
         return self
+#    def __next__(self):
+#        x = self.a
+#        self.a += 1
+#        return x
+
+#baba = JamboNumbers()
+#lowkey = iter(baba)
+
+#print(next(lowkey))
+#print(next(lowkey))
+#print(next(lowkey))
+
+#for x in lowkey:
+#    print(x)
+
+#Stopping Iteration
     def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
+        if self.a <= 20:
+            x = self.a
+            self.a += 1
+            return x
+        else:
+            raise StopIteration
 
 baba = JamboNumbers()
 lowkey = iter(baba)
 
-print(next(lowkey))
-print(next(lowkey))
-print(next(lowkey))
-print(next(lowkey))
-print(next(lowkey))
-print(next(lowkey))
-print(next(lowkey))
+for x in lowkey:
+    print(x)
