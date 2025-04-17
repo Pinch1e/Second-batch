32f224861c91fe834c80a1791ac66d620b4f09a2
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-17 13:58:00 +0200

    Me

---
+++ Dates.py
@@ -0,0 +1,13 @@
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+help("modules")
+
+import pandas
+print(pandas.__version__)
+
+import random
+print(dir(random))
+
+g = open("Math.py", "x")
---
+++ Math.py
@@ -0,0 +1,31 @@
+#Built in Math function
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+x = abs(-7.25) #The abs function returns the absolute (+ve) value of the specified number
+print(x)
+
+x = pow(4,3) # The pow(x, y) functions returns the value of x to the power of y
+
+print(x)
+
+import math
+
+x = math.sqrt(64) #The sqrt() method returns the square root of a number
+
+print(x)
+
+x = math.ceil(1.4) #Rounds up a number upwards to its nearest integers
+y = math.floor(1.4) #Rounds up a number downwards to its nearest integer
+
+print(x) #Returns 2
+print(y) #Returns 1
+
+dede = math.pi #Math.pi returns the value of PI
+
+print(dede)
