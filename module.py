50511a1097e54f15c2394c83beb1ec7ac64f0f6f
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-04-17 10:14:31 +0200

    Me

--- module.py
+++ module.py
@@ -4,3 +4,22 @@ module_test.greetings("Jonathan")
 
 a = module_test.person1["age"]
 print(a)
+
+#Renaming the Module
+import module_test as papi
+a = papi.person1["age"]
+print(a)
+b = papi.person1["name"]
+print(b)
+c = papi.person1["country"]
+print(c)
+
+#Built in Modules
+import platform
+x = platform.system()
+print(x)
+
+import platform
+x = dir(platform)
+
+f = open("Dates.py", "x")
